fedc550a36ce0dd402b9fe540021169297ad350f
Jeff Lupinski <lupinskij@gmail.com>
2016-12-02 11:56:59 -0700

    deploy

---
+++ js/templates.js
@@ -0,0 +1,9 @@
+var templates = {
+"surprise": function(locals) {
+var buf = [];
+var jade_mixins = {};
+var jade_interp;
+
+buf.push("<section id=\"surprise\"><img src=\"images/logo.png\"/><h1>Surprise!!!!!!</h1></section>");;return buf.join("");
+},
+}
